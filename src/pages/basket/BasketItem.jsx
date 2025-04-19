fb0d799441afc120c8f0e0a145316503998e5720
= <=>
2025-04-20 01:33:13 +0300

    Добавил счётчик цены

--- src/pages/basket/BasketItem.jsx
+++ src/pages/basket/BasketItem.jsx
@@ -1,11 +1,10 @@
-import React from 'react';
+import React, {useState} from 'react';
 import classes from "./Basket.module.css";
 import {ReactComponent as PlusSvg} from "./plus.svg";
 import {ReactComponent as MinusSvg} from "./minus.svg";
 import {ReactComponent as RemoveSvg} from "./delete.svg";
 
 const BasketItem = (item) => {
-    console.log(item.props);
     /*const removeItemFromCart = () => {
         /!*let count = parseInt(sessionStorage.getItem('basketCount'));
         if (count > 0) {
@@ -18,24 +17,39 @@ const BasketItem = (item) => {
         window.dispatchEvent(event);
     }*/
 
+
+    const [count, setCount] = useState(1);
+
+    function handlePlus() {
+        setCount(count + 1)
+        item.editPrice(parseInt(item.props.price));
+    }
+
+    function handleMinus() {
+        if (count > 1) {
+            setCount(count - 1)
+            item.editPrice(parseInt(item.props.price)*(-1));
+        }
+    }
+
     return (
         <div className={classes.item}>
             <div className={classes.img__container}>
                 <img className={classes.img} src={item.props.img} alt={item.props.title}/>
                 <div className={classes.item__counter__div}>
-                    <MinusSvg className={classes.item__svg}></MinusSvg>
-                    <label className={classes.item__counter__text}>1</label>
-                    <PlusSvg className={classes.item__svg}></PlusSvg>
+                    <MinusSvg className={classes.item__svg} onClick={handleMinus}></MinusSvg>
+                    <label className={classes.item__counter__text}>{count}</label>
+                    <PlusSvg className={classes.item__svg} onClick={handlePlus}></PlusSvg>
                 </div>
             </div>
             <div className={classes.item__info__div}>
                 <label className={classes.item__name}>{item.props.title}</label>
-                <label className={classes.item__price}>{item.props.price}</label>
+                <label className={classes.item__price}>{item.props.price} ₽</label>
 
             </div>
             <div className={classes.item__price__remove__div}>
                 <RemoveSvg className={classes.item__svg}></RemoveSvg>
-                <label className={classes.item__price} style={{color: "black"}}>{item.props.price}</label>
+                <label className={classes.item__price} style={{color: "black"}}>{item.props.price * count} ₽</label>
             </div>
         </div>
     );
--- src/pages/basket/BasketItems.jsx
+++ src/pages/basket/BasketItems.jsx
@@ -1,20 +1,33 @@
-import React from 'react';
+import React, {useState} from 'react';
 import classes from "./Basket.module.css";
 import BasketItem from "./BasketItem";
 
 const BasketItems = (basket) => {
+
+    const [counter, setCounter] = useState(0);
+
+    let startCounter = 0;
+
+    basket.props.forEach(item => {
+        startCounter += parseInt(item.price);
+    })
+
+    function editPrice(price) {
+        setCounter(counter + price);
+    }
+
     return (
         <div className={classes.page}>
             <div className={classes.items}>
                 {basket.props.map((item) =>
-                    <BasketItem key={item.id} props={item} />
+                    <BasketItem key={item.id} props={item} editPrice={editPrice} />
                 )}
             </div>
             <div className={classes.result__quantity}>
                 <div className={classes.box}>
                     <div className={classes.final__price__div}>
                         <label className={classes.label__text}>ИТОГО</label>
-                        <label className={classes.item__price}>40872</label>
+                        <label className={classes.final__price}>{startCounter + counter} ₽</label>
                     </div>
                     <button className={classes.button}>Перейти к оформлению</button>
                 </div>
